4639933a58a4e650d4fa2df28c9fae1e730479f4
Josemiles-ctr <josephotai37@gmail.com>
2026-06-12 09:29:25 +0300

    Booleans

---
+++ fridayBooleans(12th).py
@@ -0,0 +1,13 @@
+a,b= True, False
+
+#true and true is true
+print(a and a)
+
+# true or false is true
+print(a or b)
+
+# false and false is true
+print(b and b)
+    
+# false or false is false
+print(b or b)
