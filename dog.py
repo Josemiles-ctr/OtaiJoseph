125b4ac85903f9cd5f670b0b2b485ffe15081cc0
Josemiles-ctr <josephotai37@gmail.com>
2026-07-01 14:37:48 +0300

    style: format code for consistency and readability

--- dog.py
+++ dog.py
@@ -1,10 +1,10 @@
 class Student:
-	def __init__(self, name, age):
-            self.name = name
-            self.age = age
-	def register(self, course):
-					print(f"{self.name} has registered for {course}.")
-				
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def register(self, course):
+        print(f"{self.name} has registered for {course}.")
 				
 joseph=Student("Joseph", 20)
 joseph.register("Mathematics")
